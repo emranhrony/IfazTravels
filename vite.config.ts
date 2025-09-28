eb40b4182d6a15f934618c7b720bd1cc77b2bdf2
emranhrony <emranhusseninbd@gmail.com>
2025-09-28 05:22:54 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Use relative base so GitHub Pages (subpath) works without extra config
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
+  base: './',
+  plugins: [react()]
+})
